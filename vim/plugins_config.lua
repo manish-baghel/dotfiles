3c8e2f895ccd68ad3dde38a6e49d57783020e2a2
fr-manishbaghel <manish.baghel@fastretailing.com>
2023-06-28 10:45:43 +0900

    merge conflict resolution

--- vim/plugins_config.lua
+++ vim/plugins_config.lua
@@ -22,7 +22,7 @@ require("nvim-tree").setup({
   },
   hijack_directories = {
     enable = false,
-    auto_open = false,
+    auto_open = true,
   },
 })
 -- vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
