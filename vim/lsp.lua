357db61c03571aef1a9927de48f5359a12995db7
manish <baghelmanish660@gmail.com>
2024-01-08 17:38:25 +0900

    move null-ls to lsp.lua

--- vim/lsp.lua
+++ vim/lsp.lua
@@ -1,3 +1,23 @@
+-- Providers
+require("mason").setup()
+
+local null_ls = require("null-ls")
+
+null_ls.setup({
+  sources = {
+    null_ls.builtins.formatting.stylua,
+    null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.formatting.gofmt,
+    -- null_ls.builtins.formatting.golines,
+    null_ls.builtins.formatting.goimports,
+    null_ls.builtins.formatting.goimports_reviser,
+    null_ls.builtins.diagnostics.eslint_d,
+    null_ls.builtins.code_actions.eslint_d,
+    null_ls.builtins.completion.spell,
+    null_ls.builtins.code_actions.gomodifytags,
+  },
+})
+
 -- """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 -- " => LSP
 -- """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
--- vim/plugins_config.lua
+++ vim/plugins_config.lua
@@ -119,31 +119,6 @@ vim.cmd([[colorscheme catppuccin]])
 -- ]])
 
 require("nvim-web-devicons").setup({})
-require("mason").setup()
-
-local null_ls = require("null-ls")
-
-null_ls.setup({
-  sources = {
-    null_ls.builtins.formatting.stylua,
-    null_ls.builtins.formatting.prettierd,
-    null_ls.builtins.formatting.gofmt,
-    -- null_ls.builtins.formatting.golines,
-    null_ls.builtins.formatting.goimports,
-    null_ls.builtins.formatting.goimports_reviser,
-    null_ls.builtins.diagnostics.eslint_d,
-    null_ls.builtins.code_actions.eslint_d,
-    null_ls.builtins.completion.spell,
-    null_ls.builtins.code_actions.gomodifytags,
-  },
-})
-
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = { "*" },
-  callback = function()
-    vim.lsp.buf.format()
-  end,
-})
 
 -- """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 -- " => lualine
