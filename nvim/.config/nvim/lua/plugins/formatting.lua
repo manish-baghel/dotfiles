b8a2881cc817df9b1a192b2b87e242e49a975b34
manish <baghelmanish660@gmail.com>
2024-01-15 12:25:46 +0900

    add json formatting

--- nvim/.config/nvim/lua/plugins/formatting.lua
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -1,37 +1,33 @@
 return {
-  "stevearc/conform.nvim",
-  dependencies = {
-    "mason.nvim"
-  },
-  cmd = "ConformInfo",
-  event = "BufWritePre",
-  keys = {
-    {
-      "<leader>ff",
-      function()
-        require("conform").format({ async = true, lsp_fallback = true, formatters = { "injected" } })
-      end,
-      mode = { "n", "v" },
-      desc = "Format Injected Langs",
-    },
-  },
-  opts = {
-    -- format = {
-    --   timeout_ms = 3000,
-    --   async = false, -- not recommended to change
-    --   quiet = false, -- not recommended to change
-    -- },
-    formatters_by_ft = {
-      lua = { "stylua" },
-      go = { "goimports", "goimports-reviser", { "gofumpt", "gofmt" } },
-      python = { "isort", "black" },
-      javascript = { { "prettierd", "prettier" } },
-      sh = { "shfmt" },
-    },
+	"stevearc/conform.nvim",
+	dependencies = {
+		"mason.nvim",
+	},
+	cmd = "ConformInfo",
+	event = "BufWritePre",
+	keys = {
+		{
+			"<leader>ff",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true, formatters = { "injected" } })
+			end,
+			mode = { "n", "v" },
+			desc = "Format Injected Langs",
+		},
+	},
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			go = { "goimports", "goimports-reviser", { "gofumpt", "gofmt" } },
+			python = { "isort", "black" },
+			javascript = { { "prettierd", "prettier" } },
+			sh = { "shfmt" },
+			json = { "jq" },
+		},
 
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_fallback = true,
-    },
-  },
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_fallback = true,
+		},
+	},
 }
