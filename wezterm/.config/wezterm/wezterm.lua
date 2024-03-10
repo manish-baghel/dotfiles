6ed0da7672913994b18ee7333da8f3cc5cc6b829
manish <baghelmanish660@gmail.com>
2024-03-10 16:50:47 +0900

    updated gaps and polybar

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -6,7 +6,7 @@ config.default_prog = { "/usr/bin/fish", "-i" }
 config.color_scheme = "Catppuccin Mocha"
 config.window_background_opacity = 0.90
 
-config.enable_scroll_bar = true
+config.enable_scroll_bar = false
 config.enable_tab_bar = false
 
 return config
