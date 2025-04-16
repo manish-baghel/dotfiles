eef77b54e638399d8fc7a5b407055aafaa920954
manish <baghelmanish660@gmail.com>
2025-04-16 17:13:19 +0900

    disable cody

---
+++ nvim/.config/nvim/lua/plugins/code-companion.lua
@@ -0,0 +1,47 @@
+return {
+	{
+		"olimorris/codecompanion.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		event = { "VeryLazy" },
+		keys = {
+			{
+				"<space>cc",
+				function()
+					vim.cmd("CodeCompanionChat Toggle")
+				end,
+			},
+		},
+		config = function()
+			require("codecompanion").setup({
+				adapters = {
+					gemini = function()
+						return require("codecompanion.adapters").extend("gemini", {
+							schema = {
+								model = {
+									default = "gemini-2.5-pro-exp-03-25",
+								},
+							},
+							env = {
+								api_key = "cmd:gpg --batch --quiet --decrypt ~/google_ai_studio_api_key.gpg",
+							},
+						})
+					end,
+				},
+				strategies = {
+					chat = {
+						adapter = "gemini",
+					},
+					inline = {
+						adapter = "gemini",
+					},
+					cmd = {
+						adapter = "gemini",
+					},
+				},
+			})
+		end,
+	},
+}
