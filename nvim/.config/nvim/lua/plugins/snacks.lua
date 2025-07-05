fa37b2071b84f783c027f66f187bd66607a4797a
manish <baghelmanish660@gmail.com>
2025-07-05 22:08:59 +0900

    added snacks which provides floaterm

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -12,21 +12,150 @@ return {
 		explorer = { enabled = true },
 		indent = { enabled = true },
 		input = { enabled = true },
-		picker = { enabled = true },
+		picker = {
+			enabled = true,
+			layout = {
+				reverse = true,
+				layout = {
+					box = "horizontal",
+					backdrop = { transparent = true, blend = 40 },
+					width = 0.8,
+					min_width = 120,
+					height = 0.8,
+					{
+						box = "vertical",
+						border = "rounded",
+						title = "{title} {live} {flags}",
+						{ win = "list", border = "bottom" },
+						{ win = "input", height = 1, border = "none" },
+					},
+					{ win = "preview", title = "{preview}", border = "rounded", width = 0.5 },
+				},
+			},
+		},
 		notifier = { enabled = true },
 		quickfile = { enabled = true },
 		scope = { enabled = true },
-		scroll = { enabled = true },
 		statuscolumn = { enabled = true },
 		words = { enabled = true },
+		lazygit = {
+			win = {
+				style = "lazygit",
+				height = 0, -- 0 means full height and width, < 1 is relative sizing
+				width = 0,
+			},
+		},
 	},
 	keys = {
+		-- pickers
+		{
+			"<leader><space>",
+			function()
+				Snacks.picker.smart()
+			end,
+			desc = "Smart Find Files",
+		},
+		{
+			"<leader>o",
+			function()
+				Snacks.picker.buffers()
+			end,
+			desc = "Buffers",
+		},
+		{
+			"<leader>:",
+			function()
+				Snacks.picker.command_history()
+			end,
+			desc = "Command History",
+		},
+		{
+			"<leader>rs",
+			function()
+				Snacks.picker.resume()
+			end,
+			desc = "Resume",
+		},
+		{
+			"<leader>vn",
+			function()
+				Snacks.picker.notifications()
+			end,
+			desc = "Notification History",
+		},
+		-- find
+		{
+			"<leader>s",
+			function()
+				Snacks.picker.files()
+			end,
+			desc = "Find Files",
+		},
+		{
+			"<leader>fg",
+			function()
+				Snacks.picker.git_files()
+			end,
+			desc = "Find Git Files",
+		},
+		{
+			"<leader>e",
+			function()
+				Snacks.picker.files({ cwd = vim.fn.stdpath("config") })
+			end,
+			desc = "Find Config File",
+		},
+		{
+			"<leader>fp",
+			function()
+				Snacks.picker.projects()
+			end,
+			desc = "Projects",
+		},
+		{
+			"<leader>fr",
+			function()
+				Snacks.picker.recent()
+			end,
+			desc = "Recent",
+		},
+		-- Grep
+		{
+			"<leader>g",
+			function()
+				Snacks.picker.grep()
+			end,
+			desc = "Grep",
+		},
+		{
+			"<leader>gb",
+			function()
+				Snacks.picker.grep_buffers()
+			end,
+			desc = "Grep Open Buffers",
+		},
+		{
+			"<leader>gs",
+			function()
+				Snacks.picker.grep_word()
+			end,
+			desc = "Visual selection or word",
+			mode = { "n", "x" },
+		},
+		-- Others
 		{
 			"<leader>z",
 			function()
 				Snacks.zen()
 			end,
 			desc = "Toggle Zen Mode",
+		},
+		{
+			"<leader>lg",
+			function()
+				Snacks.lazygit()
+			end,
+			desc = "Lazygit",
 		},
 		{
 			"<leader>Z",
